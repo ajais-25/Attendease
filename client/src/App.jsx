ca9ade4bc3918d12087f732e2ce213a5ed78d49a
AKSAR <akshatjaiswal2518@gmail.com>
2024-11-08 16:52:18 +0530

    changed routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,48 @@
-import { useState } from 'react'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout";
+import NotFound from "./pages/NotFound";
+import Classes from "./pages/Classes";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Analytics from "./pages/Analytics";
+import { Provider } from "react-redux";
+import store, { persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import Report from "./pages/Report";
+import DetailedAnalytics from "./pages/DetailedAnalytics";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<SignUp />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="" element={<div>Home</div>} />
+        <Route path="classes" element={<Classes />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="analytics" element={<Analytics />} />
+        <Route path="report" element={<Report />} />
+        <Route path="detailedanalytics" element={<DetailedAnalytics />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className=''>
-        Hello
-      </div>
-    </>
-  )
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,49 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-import Layout from "./Layout";
-import NotFound from "./pages/NotFound";
-import Classes from "./pages/Classes";
-import Profile from "./pages/Profile";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import Analytics from "./pages/Analytics";
-import { Provider } from "react-redux";
-import store, { persistor } from "./store/store";
-import { PersistGate } from "redux-persist/integration/react";
-import Report from "./pages/Report";
-import DetailedAnalytics from "./pages/DetailedAnalytics";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path="login" element={<Login />} />
-      <Route path="signup" element={<SignUp />} />
-      <Route path="/" element={<Layout />}>
-        <Route path="" element={<div>Home</div>} />
-        <Route path="classes" element={<Classes />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="analytics" element={<Analytics />} />
-        <Route path="report" element= {<Report/>} />
-        <Route path="detailedanalytics" element= {<DetailedAnalytics/>} />
-      </Route>
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
-);
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <RouterProvider router={router} />
-      </PersistGate>
-    </Provider>
+    <App />
   </StrictMode>
 );
