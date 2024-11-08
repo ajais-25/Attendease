7b258917595ddf65f24a9a863d6763ef2ee373c6
PrithviS18 <prithvisen003@gmail.com>
2024-11-08 18:20:20 +0530

    corrections in report page

--- client/src/components/TopSection.jsx
+++ client/src/components/TopSection.jsx
@@ -6,16 +6,16 @@ const TopSection = ({ title }) => {
   const user = useSelector((state) => state.auth.user);
 
   return (
-    <div className="flex justify-between w-full bg-white p-4 rounded-md shadow-md mb-6">
-      {title === "report" ? <>
-        <div className="text-left">
+    <div className="w-4/6 bg-white p-4 rounded-md shadow-md mb-6 ">
+      {title === "report" ? <div className="md:flex md:justify-between ">
+        <div className="text-center md:text-start">
           <p className="sm:text-sm md:text-xl">{user && user.name}</p>
           <span className="text-gray-500 text-sm">
             {user && user.role.slice(0, 1).toUpperCase() + user.role.slice(1)}
           </span>
         </div>
-        <SearchBar/>
-      </>
+        <SearchBar />
+      </div>
         : <><h1 className="text-lg font-semibold">{title}</h1>
           <div className="text-right">
             <p>{user && user.name}</p>
