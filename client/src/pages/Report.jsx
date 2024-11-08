780fda923dd1a9fb1106dda13ecedeb6ac127461
PrithviS18 <prithvisen003@gmail.com>
2024-11-08 11:57:21 +0530

    changed report page

--- client/src/pages/Report.jsx
+++ client/src/pages/Report.jsx
@@ -19,17 +19,17 @@ export default function Report() {
         { id: "TIPSGRM1012223", name: "Swapnamoy Midya"}
     ]
     return (
-        <div className='bg-gray-100 h-screen p-6'>
+        <div className='bg-gray-100 min-h-screen flex flex-col justify-center items-center  p-6'>
             <TopSection title="report" />
 
-            <div className="p-6 bg-[#F0F7FF] shadow-md rounded-md h-[85%] ">
-                <div className="flex justify-start items-center gap-10">
+            <div className="p-6 bg-[#F0F7FF] shadow-md rounded-md md:h-[530px]">
+                <div className="flex flex-col md:flex-row justify-start items-center gap-10">
                     <span className='text-[#2B478B] text-3xl'>Attendance Report</span>
                     <span className='font-semibold text-3xl '>Total Students: 76</span>
                     <span className='font-semibold text-3xl text-[#11A529]'>Present: 56</span>
                 </div>
 
-                <div className="flex justify-start items-center mt-4 gap-10">
+                <div className="flex flex-col md:flex-row justify-start items-center mt-4 gap-10">
                     <span className='font-semibold'>
                         <p>{subject.slice(0, 22)}</p>
                         <p>{subject.slice(23,)}</p>
@@ -44,21 +44,17 @@ export default function Report() {
                         <GoClock />  {now.getHours()}:{now.getMinutes()} {now.getHours() >= 12 ? "PM" : "AM"}
                     </span>
                 </div>
-                <div className="w-full h-16 flex justify-between pl-20 pr-60 items-center bg-[#F1F1F1] mt-4 border-2 border-[#D9D9D9]">
-                    <div className="flex justify-between items-center gap-96">
-                        <span className='text-lg'>Student ID</span>
-                        <span className='text-lg'>Name</span>
-                    </div>
-                    <span className='text-lg'>Status</span>
+                <div className="sm:w-[700px] md:w-full h-10 grid grid-cols-5 bg-[#F1F1F1] mt-4 border-2 border-[#D9D9D9]">
+                    <span className='col-span-2 text-center sm:text-sm md:text-lg'>Student ID</span>
+                    <span className='col-span-2 text-center sm:text-sm md:text-lg'>Name</span>
+                    <span className='col-span-1 sm:text-sm md:text-lg rounded-md  text-center h-10'>Status</span>
                 </div>
-                <div className="overflow-y-scroll h-[60%] mt-4">
+                <div className="overflow-y-scroll md:w-full sm:overflow-x-scroll lg:overflow-x-hidden md:h-[300px] mt-4">
                     {students.map((std, index) => (
-                        <div key={index} className="w-full h-16 flex justify-between pl-20 pr-52 items-center">
-                            <div className="flex justify-between items-center gap-80">
-                                <span className='text-lg'>{std.id}</span>
-                                <span className='text-lg'>{std.name}</span>
-                            </div>
-                            <span className='text-lg bg-[#E5FFF7] p-2 text-[#0CBC8B] rounded-md w-20 text-center h-10'>Present</span> 
+                        <div key={index} className="grid grid-cols-5 h-16 ">
+                            <span className='col-span-2 sm:text-sm md:text-lg text-center'>{std.id}</span>
+                            <span className='col-span-2 sm:text-sm md:text-lg text-center'>{std.name}</span>
+                            <span className='col-span-1 sm:text-sm md:text-lg bg-[#E5FFF7] p-2 text-[#0CBC8B] rounded-md text-center h-10'>Present</span> 
                         </div>
                     ))}
                 </div>
