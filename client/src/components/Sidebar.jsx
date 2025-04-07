6f9b8fa6b2a0eb0ca2b9175195c9aaa44eb4bde8
AKSAR <akshatjaiswal2518@gmail.com>
2025-04-07 21:17:21 +0530

    made some changes in sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 import {
@@ -15,6 +16,15 @@ const Sidebar = () => {
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
+  };
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(`${API}/user/logout`, { withCredentials: true });
+    } catch (error) {
+      console.log("Error logging out: ", error);
+      console.error(error);
+    }
   };
 
   return (
@@ -54,48 +64,68 @@ const Sidebar = () => {
       <div className="menu_container flex flex-col justify-between h-[calc(100%-140px)]">
         <ul className="flex flex-col gap-6">
           <li className="item">
-            <a
-              href="#"
-              className="link flex items-center gap-4 px-4 py-2 text-white text-lg rounded-full bg-[#E0E7FF] hover:bg-[#D1D5DB] transition-all"
+            <NavLink
+              to="/dashboard"
+              className={({ isActive }) =>
+                isActive
+                  ? "link flex items-center gap-4 px-4 py-2 text-[#4B60AB] text-lg rounded-full bg-[#E0E7FF]"
+                  : "link flex items-center gap-4 px-4 py-2 text-[#D1D5DB] text-lg hover:text-black hover:bg-[#E0E7FF] rounded-full transition-all"
+              }
+              // className="link flex items-center gap-4 px-4 py-2 text-white text-lg rounded-full bg-[#E0E7FF] hover:bg-[#D1D5DB] transition-all"
             >
               <i className="text-xl">
                 <FaChartLine />
               </i>
               {isOpen && <span className="whitespace-nowrap">Dashboard</span>}
-            </a>
+            </NavLink>
           </li>
           <li className="item">
-            <a
-              href="#"
-              className="link flex items-center gap-4 px-4 py-2 text-[#D1D5DB] text-lg hover:text-black hover:bg-[#E0E7FF] rounded-full transition-all"
+            <NavLink
+              to="/classes"
+              className={({ isActive }) =>
+                isActive
+                  ? "link flex items-center gap-4 px-4 py-2 text-[#4B60AB] text-lg rounded-full bg-[#E0E7FF]"
+                  : "link flex items-center gap-4 px-4 py-2 text-[#D1D5DB] text-lg hover:text-black hover:bg-[#E0E7FF] rounded-full transition-all"
+              }
+              // className="link flex items-center gap-4 px-4 py-2 text-[#D1D5DB] text-lg hover:text-black hover:bg-[#E0E7FF] rounded-full transition-all"
             >
               <i className="text-xl">
                 <FaList />
               </i>
               {isOpen && <span className="whitespace-nowrap">Classes</span>}
-            </a>
+            </NavLink>
           </li>
           <li className="item">
-            <a
-              href="#"
-              className="link flex items-center gap-4 px-4 py-2 text-[#D1D5DB] text-lg hover:text-black hover:bg-[#E0E7FF] rounded-full transition-all"
+            <NavLink
+              to="/analytics"
+              className={({ isActive }) =>
+                isActive
+                  ? "link flex items-center gap-4 px-4 py-2 text-[#4B60AB] text-lg rounded-full bg-[#E0E7FF]"
+                  : "link flex items-center gap-4 px-4 py-2 text-[#D1D5DB] text-lg hover:text-black hover:bg-[#E0E7FF] rounded-full transition-all"
+              }
+              // className="link flex items-center gap-4 px-4 py-2 text-[#D1D5DB] text-lg hover:text-black hover:bg-[#E0E7FF] rounded-full transition-all"
             >
               <i className="text-xl">
                 <FaChartLine />
               </i>
               {isOpen && <span className="whitespace-nowrap">Analytics</span>}
-            </a>
+            </NavLink>
           </li>
           <li className="item">
-            <a
-              href="#"
-              className="link flex items-center gap-4 px-4 py-2 text-[#D1D5DB] text-lg hover:text-black hover:bg-[#E0E7FF] rounded-full transition-all"
+            <NavLink
+              to="/profile"
+              className={({ isActive }) =>
+                isActive
+                  ? "link flex items-center gap-4 px-4 py-2 text-[#4B60AB] text-lg rounded-full bg-[#E0E7FF]"
+                  : "link flex items-center gap-4 px-4 py-2 text-[#D1D5DB] text-lg hover:text-black hover:bg-[#E0E7FF] rounded-full transition-all"
+              }
+              // className="link flex items-center gap-4 px-4 py-2 text-[#D1D5DB] text-lg hover:text-black hover:bg-[#E0E7FF] rounded-full transition-all"
             >
               <i className="text-xl">
                 <FaUserCircle />
               </i>
               {isOpen && <span className="whitespace-nowrap">My Profile</span>}
-            </a>
+            </NavLink>
           </li>
         </ul>
 
@@ -104,6 +134,7 @@ const Sidebar = () => {
           className={`flex items-center gap-4 mt-auto px-5 py-1 text-[#F97316] text-lg hover:text-white hover:bg-[#F97316] rounded-full transition-all cursor-pointer ${
             isOpen ? "justify-start" : "justify-center"
           }`}
+          onClick={handleLogout}
         >
           <FaSignOutAlt className="text-xl" />
           {isOpen && <span>Sign Out</span>}
